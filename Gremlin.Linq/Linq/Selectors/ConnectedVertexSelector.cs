d12b7fdecbd1fedd796a09830bfafbfe4ec05a3d
Morgan Arvander <arvander.morgan@live.se>
2018-06-12 21:13:24 +0200

    Updated selector

--- Gremlin.Linq/Linq/Selectors/ConnectedVertexSelector.cs
+++ Gremlin.Linq/Linq/Selectors/ConnectedVertexSelector.cs
@@ -11,7 +11,7 @@
 
         public override string BuildGremlinQuery()
         {
-            return ParentSelector.BuildGremlinQuery() + $".out('{_relation}')";
+            return ParentSelector.BuildGremlinQuery() + $".out('{_relation}').hasLabel('{typeof(T).Name}')";
         }
     }
 }

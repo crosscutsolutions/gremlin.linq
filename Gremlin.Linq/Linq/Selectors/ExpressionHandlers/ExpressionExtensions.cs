08d390955ebfdd7d609a09b3ae17c5af8c0265e6
Cj Stremick <w31139@watcocompanies.com>
2019-03-22 13:36:33 -0500

    WhereIn should respect GremlinProperty names

---
+++ Gremlin.Linq/Linq/Selectors/ExpressionHandlers/ExpressionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace Gremlin.Linq.Linq.Selectors.ExpressionHandlers
+{
+    public static class ExpressionExtensions
+    {
+        public static string GetGremlinPropertyName(this MemberInfo memberInfo)
+        {
+            var propertyName = memberInfo.Name;
+            if (memberInfo.GetCustomAttributes(false).AsEnumerable().OfType<GremlinPropertyAttribute>()
+                .SingleOrDefault() is GremlinPropertyAttribute propertyAttribute)
+                propertyName = propertyAttribute.Name;
+            return propertyName;
+        }
+    }
+}

19537c0a2ad07deaf455b34c24060ab3f52f93d2
Cj Stremick <w31139@watcocompanies.com>
2019-04-02 14:49:53 -0500

    Add Vertex tests

--- Gremlin.Linq/Vertex.cs
+++ Gremlin.Linq/Vertex.cs
@@ -1,12 +1,14 @@
-﻿namespace Gremlin.Linq
+﻿using System;
+
+namespace Gremlin.Linq
 {
-    using System;
     public class Vertex : IGremlinEntity, IComparable<Vertex>
     {
-        public string Id { get; set; }
         public int CompareTo(Vertex other)
         {
-            return String.Compare(this.Id, other.Id, StringComparison.Ordinal);
+            return string.Compare(Id, other.Id, StringComparison.Ordinal);
         }
+
+        public string Id { get; set; }
     }
 }
